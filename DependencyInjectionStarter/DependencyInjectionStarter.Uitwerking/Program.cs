176168bcd43f5e5ed92043ba3bf0531ba7892a88
Stino <linksonder@gmail.com>
2017-11-08 11:09:32 +0100

    uitwerking

---
+++ DependencyInjectionStarter/DependencyInjectionStarter.Uitwerking/Program.cs
@@ -0,0 +1,25 @@
+﻿using DependencyInjectionStarter.Library;
+using System;
+using System.Collections.Generic;
+
+namespace DependencyInjectionStarter
+{
+    class Program
+    {
+        /** Testing 123 */
+        static void Main()
+        {
+            List<IInstrument> instrumenten = new List<IInstrument>();
+            instrumenten.Add(new Guitar());
+            instrumenten.Add(new Guitar());
+            instrumenten.Add(new BassGuitar());
+            instrumenten.Add(new Drums());
+            instrumenten.Add(new Vocal());
+
+
+            var rockBand = new RockBand(instrumenten);
+            rockBand.DoSoundCheck();
+            Console.ReadLine();
+        }
+    }
+}
