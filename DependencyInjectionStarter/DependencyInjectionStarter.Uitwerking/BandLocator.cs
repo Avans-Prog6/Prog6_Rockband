10f7e8d7d6c98153a4150dfc2cee4b341cd98042
Stino <linksonder@gmail.com>
2017-11-12 18:38:12 +0100

    Uitwerking laatste commit

--- DependencyInjectionStarter/DependencyInjectionStarter.Uitwerking/BandLocator.cs
+++ DependencyInjectionStarter/DependencyInjectionStarter.Uitwerking/BandLocator.cs
@@ -14,12 +14,12 @@ namespace DependencyInjectionStarter
 
         public BandLocator()
         {
+            _kernel = new StandardKernel();
             _kernel.Bind<RockBand>().ToSelf().InSingletonScope();
             _kernel.Bind<IInstrument>().To<Guitar>();
             _kernel.Bind<IInstrument>().To<Vocal>();
-            _kernel.Bind<IInstrument>().To<Guitar>();
-            _kernel.Bind<IInstrument>().To<Guitar>();
-
+            _kernel.Bind<IInstrument>().To<BassGuitar>();
+            _kernel.Bind<IInstrument>().To<Drums>();
         }
 
         public RockBand DefaultRockband
